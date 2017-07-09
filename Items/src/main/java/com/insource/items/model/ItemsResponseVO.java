1e24c0d85b732dbc423754916579702974f74661
Manojchandrakumar.S <Manojchandrakumar.S@MANOJ-PC>
2017-07-09 16:07:32 +0530

    Code Rebase 2

---
+++ Items/src/main/java/com/insource/items/model/ItemsResponseVO.java
@@ -0,0 +1,24 @@
+package com.insource.items.model;
+
+public class ItemsResponseVO {
+
+	private boolean status;
+	private String serviceMessage;
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public String getServiceMessage() {
+		return serviceMessage;
+	}
+
+	public void setServiceMessage(String serviceMessage) {
+		this.serviceMessage = serviceMessage;
+	}
+
+}
